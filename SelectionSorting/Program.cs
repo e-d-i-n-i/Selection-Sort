6be09ef0ca0cc50cf6077aa782ef82ae68990d95
Edini Amare Getachew <122861398+e-d-i-n-i@users.noreply.github.com>
2023-09-15 03:23:42 +0300

    Add files via upload

---
+++ SelectionSorting/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[7];
+
+            // Input
+            Console.WriteLine("Enter 7 numbers: ");
+            for (int i = 0; i < 7; ++i)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            // Selection Sort
+            for (int i = 0; i < 6; ++i)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < 7; ++j)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+
+            // Sorted Output
+            Console.Write("Sorted numbers: ");
+            foreach (int num in arr)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            return;
+        }
+    }
+}
